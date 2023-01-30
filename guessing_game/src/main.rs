0826b34f49ec2858ebc9e0bced95c89565cb5420
anassohail99 <anas.sohailabs1@gmail.com>
2023-01-30 19:30:39 +0500

    ✨ fetch random numbers

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,9 +1,13 @@
+use rand::Rng;
 use std::io;
-
 
 fn main() {
     println!("Guess the number!");
     println!("Please input you guessed number: ");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    println!("the secret number is {secret_number}");
 
     let mut guesss = String::new();
 
